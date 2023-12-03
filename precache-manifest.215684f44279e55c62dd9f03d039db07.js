b2a5884514979611f066ec38b319d1e2b1994e1b
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2023-12-03 17:52:31 -0500

    create site

---
+++ precache-manifest.215684f44279e55c62dd9f03d039db07.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22faae2f08605e1b24ee3c90ded8a98b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "366bd99241f02c7d19d6",
+    "url": "./static/css/2.950e20d2.chunk.css"
+  },
+  {
+    "revision": "d47db8aabf2ad51f7bbb",
+    "url": "./static/css/main.71db1769.chunk.css"
+  },
+  {
+    "revision": "366bd99241f02c7d19d6",
+    "url": "./static/js/2.5d299aea.chunk.js"
+  },
+  {
+    "revision": "c5f92507b76b3fbc3f8e5f5fe0c84cf3",
+    "url": "./static/js/2.5d299aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d47db8aabf2ad51f7bbb",
+    "url": "./static/js/main.6e177306.chunk.js"
+  },
+  {
+    "revision": "796be32ebbb07525fb52",
+    "url": "./static/js/runtime-main.d39248e9.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "./static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "e631f0ca65031b20fe23668e166aa85b",
+    "url": "./static/media/remove.e631f0ca.svg"
+  },
+  {
+    "revision": "a700735566a3701072afbd164b80e690",
+    "url": "./static/media/resize.a7007355.svg"
+  }
+]);
